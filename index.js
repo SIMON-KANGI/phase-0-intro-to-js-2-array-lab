86b86bf838e85c7406df825cef66570e32592365
SIMON-KANGI <simonmwangikangi@gmail.com>
2023-12-14 18:22:51 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+let cats=["Milo","Otis", "Garfield"]
+const destructivelyAppendCat=(name)=>{
+return cats.push(name)
+}
+const destructivelyPrependCat=(name)=>{
+return cats.unshift(name)
+}
+const destructivelyRemoveLastCat=()=>{
+    return cats.pop()
+}
+const destructivelyRemoveFirstCat=()=>{
+    return cats.shift()
+}
+const appendCat=(name)=>{
+    let newArr= cats.concat(name)
+    return newArr
+}
+const prependCat=(name)=>{
+    let newArr=[name,...cats]
+    return newArr
+}
+const removeLastCat=()=>{
+    let newArr=[...cats]
+    return newArr.slice(0,2)
+    
+}
+console.log(removeLastCat())
+const removeFirstCat=()=>{
+    let newArr= [...cats]
+    return newArr.slice(1,3)
+}
+console.log(removeFirstCat())
